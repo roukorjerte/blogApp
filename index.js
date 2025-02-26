261380891c795aed801a178d11dd8c568c068dfd
Diana <54017944+roukorjerte@users.noreply.github.com>
2025-02-26 18:20:18 +0200

    Add files via upload
    created placeholders as objects in json file, linked them to the all posts page, created new template for individual posts, changes js file so that I can visit each individual post

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import express from "express";
+import fs from "fs";
+
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.use(express.static("public"));
 
+const articles = JSON.parse(fs.readFileSync("articles.json", "utf8"));
+
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { articles });
 });
 
 app.get("/all-posts", (req, res) => {
-  res.render("allPosts.ejs");
+  res.render("allPosts.ejs", { articles });
 });
 
 app.get("/create", (req, res) => {
@@ -19,6 +27,15 @@ app.get("/create", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
+});
+
+app.get("/articles/:id", (req, res) => {
+  const article = articles.find(a => a.id == req.params.id);
+  if (article) {
+      res.render("article", { article });
+  } else {
+      res.status(404).send("Статья не найдена");
+  }
 });
 
 app.listen(port, () => {
